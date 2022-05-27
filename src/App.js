54e7fd2c038dbf364d18146660208f730d24a9a0
rounit08 <rounitrsinha@gmail.com>
2022-05-27 19:55:30 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import Explore from "./components/Explore";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/explore" element={<Explore />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+        </Routes>
+      </div>
+
+      <div className="header">
+        <Link to="/">
+          <button className="homebtn">HOME</button>
+        </Link>{" "}
+        <Link to="/explore">
+          <button className="explorebtn">EXPLORE</button>
+        </Link>{" "}
+        <Link to="/profile">
+          <button className="profilebtn">PROFILE</button>
+        </Link>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Explore.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Explore() {
+  return (
+    <div>
+      <input type="text" />
+      <button>SUBMIT</button>{" "}
+    </div>
+  );
+}
+
+export default Explore;
